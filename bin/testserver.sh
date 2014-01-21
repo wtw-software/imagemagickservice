40ea84ebba3743aff9b354fd7f225897eb7fbca3
Andre Tangen @gorillatron <jornandretangen@gmail.com>
2014-01-21 16:00:42 +0100

    tet files

---
+++ bin/testserver.sh
@@ -0,0 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+i="0"
+
+while [ $i -lt 50 ] 
+do
+
+  curl "localhost:3000/api/convert%20-resize%20100x100" -F myfile=@"$DIR/blackhole.png" > /dev/null 2> /dev/null & 
+  i=$[$i+1]
+done
