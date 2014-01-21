edc8c5fc3866b7fb7adc3c80be72d183d44a6c7f
Andre Tangen @gorillatron <jornandretangen@gmail.com>
2014-01-21 16:01:12 +0100

    Better pattern

--- app.js
+++ app.js
@@ -9,19 +9,31 @@ var express       = require( 'express' ),
     fs            = require( 'fs' ),
     path          = require( 'path' ),
     Jobqueue      = require( './lib/Jobqueue' ),
-    api           = require( './api' )
+    LoadLimiter   = require( './lib/LoadLimiter' ),
+    api           = require( './routes/api' )
 
 
-var app       = express(),
-    jobqueue  = new Jobqueue({ concurrency: 4 })
 
-
+/**
+ * App, global namespace
+ */
+app = express()
 module.exports = app
 
+
+/**
+ * Singeltons
+ */
+app.jobqueue    = new Jobqueue({ concurrency: 8 }),
+app.loadlimiter = new LoadLimiter({ maxConcurrentRequests: 20 })
+
+
+/**
+ * Config
+ */
 app.set( 'port', process.env.PORT || 3000 )
 app.set( 'views', path.join(__dirname, 'views') )
 app.set( 'view engine', 'jade' )
-app.set( 'jobqueue', jobqueue )
 app.use( express.favicon() )
 app.use( express.logger('dev') )
 app.use( express.json() )
@@ -30,11 +42,26 @@ app.use( express.methodOverride() )
 app.use( app.router )
 app.use( express.static(path.join(__dirname, 'public')) )
 
-
 if( 'development' == app.get('env') ) {
   app.use( express.errorHandler() )
 }
 
+
+/**
+ * Middleware
+ */
+var interceptUploadStream = require( './middleware/interceptUploadStream' ),
+    parseTerminalParams   = require( './middleware/parseTerminalParams' )
+
+
+/**
+ * Routes
+ */
 app.get( '/', routes.index )
 
-api( app )
+app.all( '/api/*', app.loadlimiter.createMiddleware() )
+
+app.post( /\/api\/convert(.+)/, interceptUploadStream({ timeout: 5000 }), parseTerminalParams, api.convert )
+
+
+
