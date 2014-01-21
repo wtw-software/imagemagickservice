42333254aa3a448aa3df054b2cdbc03414847ffa
Andre Tangen @gorillatron <jornandretangen@gmail.com>
2014-01-21 10:35:34 +0100

    startserver script

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ var app       = express(),
     jobqueue  = new Jobqueue({ concurrency: 4 })
 
 
+module.exports = app
+
 app.set( 'port', process.env.PORT || 3000 )
 app.set( 'views', path.join(__dirname, 'views') )
 app.set( 'view engine', 'jade' )
@@ -36,7 +38,3 @@ if( 'development' == app.get('env') ) {
 app.get( '/', routes.index )
 
 api( app )
-
-http.createServer( app ).listen(app.get( 'port' ), function(){
-  console.log( 'Express server listening on port ' + app.get('port') )
-})
